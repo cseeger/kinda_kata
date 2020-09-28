f71ae0c76ade17acf6c8664f2e75e39272d224e5
Chad Seeger <90306+cseeger@users.noreply.github.com>
2020-09-28 16:36:23 -0700

    `encode` test

--- elixir/base62/test/base62_test.exs
+++ elixir/base62/test/base62_test.exs
@@ -1,8 +1,11 @@
 defmodule Base62Test do
   use ExUnit.Case
-  doctest Base62
 
-  test "greets the world" do
-    assert Base62.hello() == :world
+  describe "encode/1" do
+    test "returns the base62 encoding string" do
+      assert Base62.encode(1) == "b"
+      assert Base62.encode(125) == "cb"
+      assert Base62.encode(99262) == "zZa"
+    end
   end
 end
