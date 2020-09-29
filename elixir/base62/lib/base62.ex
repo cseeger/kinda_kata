047c4791fe5f92ae49b7f73be2dc90b29b2399fb
Chad Seeger <90306+cseeger@users.noreply.github.com>
2020-09-28 20:07:18 -0700

    Flesh out `Base64`

--- elixir/base62/lib/base62.ex
+++ elixir/base62/lib/base62.ex
@@ -1,23 +1,46 @@
 defmodule Base62 do
-  @moduledoc """
-  Base62 encoder
-  Inspired from: (rejoin url)
-    https://github.com/otobus/event_bus/commit/
-    956ffd93d854fb3a721aa7763c3da509cffedd41
-    #diff-31f9094877d525a1b8387d4135042006R1
-  """
+  # discussion:
+  # https://stackoverflow.com/questions/742013/how-do-i-create-a-url-shortener
+  #
+  # with code from:
+  # https://github.com/otobus/event_bus/commit/
+  # 956ffd93d854fb3a721aa7763c3da509cffedd41
+  # diff-31f9094877d525a1b8387d4135042006R1
+  #
+  # also, decode algorithm from "Horner's Rule"
 
   @mapping 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+  @radix 62
 
-  @doc """
-  Converts given integer to base62
-  """
-  @spec encode(integer()) :: String.t()
-  def encode(num) when num < 62 do
+  def encode(num) when num < @radix do
     << Enum.at(@mapping, num) >>
   end
 
   def encode(num) do
-    encode(div(num, 62)) <> encode(rem(num, 62))
+    encode(div(num, @radix)) <> encode(rem(num, @radix))
+  end
+
+  def decode(encoded) do
+    String.to_charlist(encoded)
+    |> char_indexes()
+    |> accumulate_decode(0)
+    |> trunc()
+  end
+
+  def accumulate_decode(chars, acc) when length(chars) == 0, do: acc
+
+  def accumulate_decode(chars, acc) do
+    square = :math.pow(@radix, length(chars) - 1)
+    [head | remaining_chars] = chars
+    result = head * square
+    acc = acc + result
+
+    accumulate_decode(remaining_chars, acc)
+  end
+
+  def char_indexes(chars) do
+    Enum.map(chars, fn x ->
+      Enum.find_index(@mapping, &(&1 == x))
+    end)
   end
 end
--- elixir/base62/test/base62_test.exs
+++ elixir/base62/test/base62_test.exs
@@ -3,9 +3,33 @@ defmodule Base62Test do
 
   describe "encode/1" do
     test "returns the base62 encoding string" do
+      assert Base62.encode(0) == "a"
       assert Base62.encode(1) == "b"
       assert Base62.encode(125) == "cb"
       assert Base62.encode(99262) == "zZa"
+    end
+  end
+
+  describe "decode/1" do
+    test "returns the char map as string" do
+      assert Base62.decode("a") == 0
+      assert Base62.decode("b") == 1
+      assert Base62.decode("cb") == 125
+      assert Base62.decode("zZa") == 99262
+    end
+  end
+
+  describe "char_indexes/1" do
+    test "returns the nums for the given alpha_chars" do
+      assert Base62.char_indexes('cb') == [2, 1]
+      assert Base62.char_indexes('zZa') == [25, 51, 0]
+    end
+  end
+
+  describe "accumulate_decode/2" do
+    test "returns the base10 from given base62" do
+      assert Base62.accumulate_decode([2, 1], 0) == 125
+      assert Base62.accumulate_decode([25, 51, 0], 0) == 99262
     end
   end
 end
